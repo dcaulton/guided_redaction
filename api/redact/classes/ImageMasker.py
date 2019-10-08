3ca5a533410c83188b013beee81d6ce92722635a
Dave Caulton <dcaulton@gmail.com>
2019-10-08 15:32:06 -0500

    provide three different masking methods

--- api/redact/classes/ImageMasker.py
+++ api/redact/classes/ImageMasker.py
@@ -6,9 +6,19 @@ class ImageMasker():
     def __init__(self):
         pass
 
-    def mask_all_regions(self, source, regions_to_mask):
+    def mask_all_regions(self, source, regions_to_mask, mask_info):
         output = source.copy()
         for region_number, masking_region in enumerate(regions_to_mask):
-            print('masking region', masking_region)
-            cv2.rectangle(output, masking_region[0], masking_region[1], (0, 255, 0), 2)
+            mask_method = mask_info.get('method', 'black_rectangle')
+            if mask_method == 'black_rectangle':
+                cv2.rectangle(output, masking_region[0], masking_region[1], (0, 0, 0), -1)
+            elif mask_method == 'green_outline':
+                cv2.rectangle(output, masking_region[0], masking_region[1], (0, 255, 0), 2)
+            elif mask_method == 'blur_7x7':
+                start_x = masking_region[0][0]
+                end_x = masking_region[1][0]
+                start_y = masking_region[0][1]
+                end_y = masking_region[1][1]
+                output[start_y:end_y, start_x:end_x] = \
+                    cv2.blur(output[start_y:end_y, start_x:end_x], (7, 7))
         return output
--- api/redact/views.py
+++ api/redact/views.py
@@ -17,12 +17,13 @@ def index(request):
 
             data = json.loads(request.POST.get('data'))
             areas_to_redact_from_json = data.get('areas_to_redact', [])
+            mask_info = data.get('mask_info', {"method": "black_rectangle"})
             areas_to_redact = []
             for a2r in areas_to_redact_from_json:
                 areas_to_redact.append([tuple(a2r[0]), tuple(a2r[1])])
 
             image_masker = ImageMasker()
-            masked_image = image_masker.mask_all_regions(cv2_image, areas_to_redact)
+            masked_image = image_masker.mask_all_regions(cv2_image, areas_to_redact, mask_info)
             
             #TODO stream an image back to the caller 
             cv2.imwrite('/Users/dcaulton/Desktop/howboutthat.png', masked_image)
