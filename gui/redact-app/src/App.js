4d9e6d90db107b447d77db3c7a7bcd7577b5ef06
Dave Caulton <dcaulton@gmail.com>
2019-10-24 15:50:45 -0500

    break TopControls out into its own class

--- gui/redact-app/src/App.js
+++ gui/redact-app/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Thing from './components/Thing';
+import TopControls from './components/TopControls';
 import './App.css';
 
 class App extends React.Component {
@@ -152,120 +154,6 @@ class ImageCanvas extends React.Component {
     return (
       <div id='canvas_div'>
         <canvas id='overlay_canvas' />
-      </div>
-    );
-  }
-}
-
-class TopControls extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      mode: props.mode,
-      submode: props.submode,
-      display_mode: props.display_mode,
-      message: props.message,
-      debuginfo: props.debuginfo,
-      areas_to_redact: props.areas_to_redact,
-    };
-  }
-
-  render() {
-    return (
-      <div id='bottom_controls_div'>
-        <div className='row'>
-          <div className='col-md-1' />
-          <div className='col' id='add_div'>
-            <button className='btn btn-primary dropdown-toggle' type='button' id='addDropdownButton'
-                data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
-              Add
-            </button>
-            <div className='dropdown-menu' aria-labelledby='addDropdownButton'>
-              <button className='dropdown-item' 
-                  onClick={() => this.props.setModeCallback('add_1', 'box')}>
-                Box
-                {this.state.debuginfo}
-              </button>
-              <button className='dropdown-item' 
-                  onClick={() => this.props.setModeCallback('add_1', 'ocr')} 
-                  href='.'>
-                OCR
-              </button>
-{/*
-              <button className='dropdown-item' 
-                  onClick={() => this.props.setModeCallback('ADD', 'flood')} 
-                  href='.'>
-                Flood
-              </button>
-              <button className='dropdown-item' 
-                  onClick={() => this.props.setModeCallback('ADD', 'polyline')} 
-                  href='.'>
-                Polyline
-              </button>
-*/}
-            </div>
-          </div>
-          <div className='col' id='delete_div'>
-            <button className='btn btn-primary dropdown-toggle' type='button' id='deleteDropdownButton'
-                data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
-              Delete
-            </button>
-            <div className='dropdown-menu' aria-labelledby='deleteDropdownButton'>
-              <button className='dropdown-item' 
-                  onClick={() => this.props.setModeCallback('delete', 'item')} 
-                  href='.'>
-                Item
-              </button>
-              <button className='dropdown-item' 
-                  onClick={() => this.props.setModeCallback('delete_1', 'box_all')} 
-                  href='.'>
-                Box (all in)
-              </button>
-{/*
-              <button className='dropdown-item' 
-                  onClick={() => this.props.setModeCallback('DELETE', 'box_part')} 
-                  href='.'>
-                Box (part in)
-              </button>
-*/}
-            </div>
-          </div>
-          <div className='col'>
-            <button 
-                className='btn btn-primary' 
-                onClick={() => this.props.setModeCallback('view', '')}
-                href='./index.html' >
-              Cancel
-            </button>
-          </div>
-          <div className='col'>
-            <button 
-                className='btn btn-primary' 
-                onClick={() => this.props.setModeCallback('reset', '')}
-                href='./index.html' >
-              Reset Image
-            </button>
-          </div>
-          <div className='col'>
-            <button 
-                className='btn btn-primary'  
-                onClick={() => this.props.setModeCallback('redact', '')}
-                href='./index.html' >
-              Redact
-            </button>
-          </div>
-          <div className='col marquee_div'>
-            <span>hiya</span>
-          </div>
-        </div>
-        <div className='row d-flex justify-content-between'>
-          <div id='mode_div' className='col-md-4'>
-            <h3 id='mode_header' >{this.props.display_mode}</h3>
-          </div>
-          <div id='message_div' className='pt-2 col-md-8'>
-            <span id='message'>{this.props.message}</span>
-          </div>
-        </div>
       </div>
     );
   }
