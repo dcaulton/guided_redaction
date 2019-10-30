19daeb262950eac2a6042cf2b251f3c4d104067a
Dave Caulton <dcaulton@gmail.com>
2019-10-30 12:29:32 -0500

    develop a card layout for frames

--- gui/redact-app/src/components/MovieParserPanel.js
+++ gui/redact-app/src/components/MovieParserPanel.js
@@ -6,12 +6,15 @@ class MovieParserPanel extends React.Component {
     super(props)
     this.state = {
       frame_frameset_view_mode: 'frame',
+      frames: [],
+      framesets: {},
     }
   }
 
   componentDidMount() {
     document.getElementById('frame_inactive').style.display = 'none';
     document.getElementById('frameset_active').style.display = 'none';
+    this.showFrames();
   }
 
   showFrameSets() {
@@ -71,36 +74,56 @@ class MovieParserPanel extends React.Component {
           </div>
         </div>
         <div id='frame_and_frameset_data' className='row mt-3'>
-          <div id='frame_frameset_header'>
-            <div id='ffh_frame_title'>
-              <button
-                  id='frame_active'
-                  className='btn btn-secondary'
-              >
-                Displaying all Frames
-              </button>
-              <button
-                  id='frame_inactive'
-                  className='btn btn-primary'
-                  onClick={() => this.toggleFrameFramesetViewMode()}
-              >
-                Show Frames
-              </button>
+          <div id='frame_frameset_header' className='col-md-12'>
+            <div className='row container'>
+              <div id='ffh_frame_title' className='col-md-3'>
+                <button
+                    id='frame_active'
+                    className='btn btn-secondary'
+                >
+                  Displaying all Frames
+                </button>
+                <button
+                    id='frame_inactive'
+                    className='btn btn-primary'
+                    onClick={() => this.toggleFrameFramesetViewMode()}
+                >
+                  Show Frames
+                </button>
+              </div>
+              <div id='ffh_frameset_title' className='col-md-3'>
+                <button
+                    id='frameset_active'
+                    className='btn btn-secondary'
+                >
+                  Displaying all FrameSets
+                </button>
+                <button
+                    id='frameset_inactive'
+                    className='btn btn-primary'
+                    onClick={() => this.toggleFrameFramesetViewMode()}
+                >
+                  Show FrameSets
+                </button>
+              </div>
             </div>
-            <div id='ffh_frameset_title'>
-              <button
-                  id='frameset_active'
-                  className='btn btn-secondary'
-              >
-                Displaying all FrameSets
-              </button>
-              <button
-                  id='frameset_inactive'
-                  className='btn btn-primary'
-                  onClick={() => this.toggleFrameFramesetViewMode()}
-              >
-                Show FrameSets
-              </button>
+          </div>
+          <div id='frame_frameset_data' className='col-md-12'>
+            <div className='row m-5'>
+              <div className='col-md-2 frameCard m-3 p-3 bg-light'>
+                <h5 className='card-title'>frame_00185</h5>
+                <img src='http://127.0.0.1:3000/images/frame_00185.png' alt='whatever'/>
+                <div className='card-body'>
+                  <p className='card-text'>other stuff</p>
+                </div>
+              </div>
+              <div className='col-md-2 frameCard m-3 p-3 bg-light'>
+                <h5 className='card-title'>frame_00187</h5>
+                <img src='http://127.0.0.1:3000/images/frame_00187.png' alt='whatever'/>
+                <div className='card-body'>
+                  <p className='card-text'>stuff</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
