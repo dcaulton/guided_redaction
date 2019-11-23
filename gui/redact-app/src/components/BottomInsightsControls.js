7b72bd1d4ff976e503fda9fb8b92b38cf290bcda
Dave Caulton <dcaulton@gmail.com>
2019-11-23 13:33:24 -0600

    flesh out the buttons needed for a mature template explorer

--- gui/redact-app/src/components/BottomInsightsControls.js
+++ gui/redact-app/src/components/BottomInsightsControls.js
@@ -17,40 +17,141 @@ class BottomInsightsControls extends React.Component {
           className='fixed-bottom'
           style={controls_style}
       >
-        <div className='row bg-light'>
+
+
+
+        <div className='row bg-light rounded'>
           <div className='col'>
-            <button
-                className='btn btn-primary ml-5'
-                onClick={() => alert('fun guy')}
+            <div className='row'>
+              <div 
+                className='col-lg-10 h3'
+              > 
+                templates
+              </div>
+              <div className='col float-right'>
+                <button
+                    className='btn btn-link'
+                    aria-expanded='false'
+                    data-target='#template_controls_body'
+                    aria-controls='template_controls_body'
+                    data-toggle='collapse'
+                    type='button'
+                >
+                  show/hide
+                </button>
+              </div>
+            </div>
+
+            <div 
+                id='template_controls_body' 
+                className='row collapse'
             >
-              Set Template
-            </button>
-            <button
-                className='btn btn-primary ml-5'
-                onClick={() => this.props.setMode('add_roi_1')}
-            >
-              Add Anchor
-            </button>
-            <button
-                className='btn btn-primary ml-5'
-                onClick={() => this.props.clearRoiCallback()}
-            >
-              Clear Anchor
-            </button>
-            <button
-                className='btn btn-primary ml-5'
-                onClick={() => this.props.scanSubImage()}
-            >
-              Scan Template
-            </button>
-            <button
-                className='btn btn-primary ml-5'
-                onClick={() => this.props.clearSubImageMatches()}
-            >
-              Clear Template Matches
-            </button>
+              <div id='template_controls_main' className='col'>
+                <button
+                    className='btn btn-primary ml-2 mt-2'
+                    onClick={() => alert('new template')}
+                >
+                  New
+                </button>
+                <button
+                    className='btn btn-primary ml-2 mt-2'
+                    onClick={() => alert('set active template')}
+                >
+                  Set Active
+                </button>
+                <button
+                    className='btn btn-primary ml-2 mt-2'
+                    onClick={() => this.props.setMode('add_roi_1')}
+                >
+                  Add Anchor
+                </button>
+                <button
+                    className='btn btn-primary ml-2 mt-2'
+                    onClick={() => this.props.clearRoiCallback()}
+                >
+                  Clear Anchor
+                </button>
+                <button
+                    className='btn btn-primary ml-2 mt-2'
+                    onClick={() => alert('add template zone')}
+                >
+                  Add Zone
+                </button>
+                <button
+                    className='btn btn-primary ml-2 mt-2'
+                    onClick={() => alert('clear template zones')}
+                >
+                  Clear Zones
+                </button>
+                <button
+                    className='btn btn-primary ml-2 mt-2'
+                    onClick={() => alert('delete template')}
+                >
+                  Delete
+                </button>
+                <button
+                    className='btn btn-primary ml-2 mt-2'
+                    onClick={() => alert('set template scale')}
+                >
+                  Scale
+                </button>
+
+                <div
+                    className='d-inline ml-2 mt-2'
+                >   
+                    <input 
+                        id='template_name'
+                        size='10'
+                        value='Hist %'
+                        onChange={() => console.log('setting histogram percent')}
+                    />
+                </div>
+
+                <div
+                    className='d-inline ml-2 mt-2'
+                >   
+                    <input 
+                        id='template_name'
+                        size='10'
+                        value='Match %'
+                        onChange={() => console.log('setting template match percent')}
+                    />
+                </div>
+
+                <div
+                    className='d-inline ml-2 mt-2'
+                >   
+                    <input 
+                        id='template_name'
+                        size='25'
+                        value='Template Name'
+                        onChange={() => console.log('setting template name')}
+                    />
+                </div>
+
+                <button
+                    className='btn btn-primary ml-2 mt-2'
+                    onClick={() => this.props.scanSubImage()}
+                >
+                  Scan Template
+                </button>
+                <button
+                    className='btn btn-primary ml-2 mt-2'
+                    onClick={() => this.props.clearSubImageMatches()}
+                >
+                  Clear
+                </button>
+              </div>
+
+            </div>
           </div>
         </div>
+
+
+
+
+
+
 
         <div className='row mt-3 bg-light'>
           <div className='col'>
