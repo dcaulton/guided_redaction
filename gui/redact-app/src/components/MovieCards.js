29a05ca94055e0b614413628a61ed6630736d47d
Dave Caulton <dcaulton@gmail.com>
2019-12-28 16:22:37 -0600

    make movieCards collapsible

--- gui/redact-app/src/components/MovieCards.js
+++ gui/redact-app/src/components/MovieCards.js
@@ -55,8 +55,10 @@ class MovieCard extends React.Component {
       let nickname = movies[movie_url]['nickname']
       return_arr.push(
         <input
+            className='nickname'
             key='1122'
             value={nickname}
+            size='24'
             onChange={(event) => this.props.setMovieNickname(movie_url, event.target.value)}
         /> 
       )
@@ -102,6 +104,31 @@ class MovieCard extends React.Component {
     return ''
   }
 
+  buildMovieHeader(movie, movie_body_id, nickname_block) {
+    let style = {
+      'fontSize': 'small',
+      'padding': 0,
+    }
+    return (
+      <div
+        className='d-inline'
+      >
+        {nickname_block}
+        <button
+            style={style}
+            className='btn btn-link'
+            aria-expanded='false'
+            data-target={'#'+movie_body_id}
+            aria-controls={movie_body_id}
+            data-toggle='collapse'
+            type='button'
+        >
+          +/-
+        </button>
+      </div>
+    )
+
+  }
   render() {
     let active_status = false
     if (this.props.this_cards_movie_url === this.props.active_movie_url) {
@@ -130,6 +157,8 @@ class MovieCard extends React.Component {
     if (active_status) {
       top_div_classname = "row mt-4 card active_movie_card"
     }
+    const movie_body_id = 'movie_body_' + Math.floor(Math.random(1000000, 9999999)*1000000000).toString()
+    const movie_header = this.buildMovieHeader(this.props.movies[this.props.this_cards_movie_url], movie_body_id, nickname_block)
 
     return (
       <div 
@@ -139,36 +168,43 @@ class MovieCard extends React.Component {
           onDragOver={(event) => event.preventDefault()}
       >
         <div className='col'>
-            <div className='row'>
-              <video 
-                  id='video_{this.props.key}' 
-                  controls 
-              >
-                <source
-                  src={this.props.this_cards_movie_url}
-                  type='video/mp4'
-                />
-              </video>
+            <div className='row' id='movie_header'>
+              {movie_header}
             </div>
-          <div className='row'>
-          {nickname_block}
-          </div>
-          <div className='row'>
-            {make_active_button}
-            {load_as_job_button}
-          </div>
-          <div className='row'>
-            {framesets_count_message}
-          </div>
-          <div className='row'>
-            {found_string}
-          </div>
-          <div className='row'>
-            {selected_string}
-          </div>
-          <div className='row'>
-            {dims_string}
-          </div>
+
+            <div 
+              id={movie_body_id}
+              className='collapse show'
+            >
+              <div className='row'>
+                <video 
+                    id='video_{this.props.key}' 
+                    controls 
+                >
+                  <source
+                    src={this.props.this_cards_movie_url}
+                    type='video/mp4'
+                  />
+                </video>
+              </div>
+              <div className='row'>
+                {make_active_button}
+                {load_as_job_button}
+              </div>
+              <div className='row'>
+                {framesets_count_message}
+              </div>
+              <div className='row'>
+                {found_string}
+              </div>
+              <div className='row'>
+                {selected_string}
+              </div>
+              <div className='row'>
+                {dims_string}
+              </div>
+            </div>
+
         </div>
       </div>
     )
