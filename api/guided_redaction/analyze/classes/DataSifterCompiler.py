73a74b1135c9eab3c83b1a026db64e0d7254ee23
Dave Caulton <dcaulton@gmail.com>
2020-09-27 21:41:35 -0500

    planning for DSC wrapup

--- api/guided_redaction/analyze/classes/DataSifterCompiler.py
+++ api/guided_redaction/analyze/classes/DataSifterCompiler.py
@@ -10,13 +10,15 @@ class DataSifterCompiler(GridPointScorer):
         self.data_sifter = data_sifter
         self.movies = movies
         self.file_writer = file_writer
+        self.all_frameset_rows = {}
+        self.movie_max_frameset_rows = {}
         self.all_movie_scores = {}
         self.debug = False
         self.row_threshold = 20
         self.word_match_threshold = 80
-        self.all_frameset_rows = {}
         for movie_url in self.movies:
             self.all_frameset_rows[movie_url] = {}
+            self.movie_max_frameset_rows = {}
             self.all_movie_scores[movie_url] = {}
 
     def compile(self):
@@ -32,12 +34,30 @@ class DataSifterCompiler(GridPointScorer):
             max_frameset_hash = self.get_frameset_hash_with_max_rows(movie_url)
 
             for frameset_hash in self.movies[movie_url]['framesets']:
-                self.score_frameset(max_frameset_hash, movie_url, frameset_hash)
+                best_score_obj = self.score_frameset(
+                    max_frameset_hash, movie_url, frameset_hash
+                )
+                self.all_movie_scores[movie_url][frameset_hash] = best_score_obj
 
-#            print('MAXIMAL frameset data')
-#            print(self.all_frameset_rows[movie_url][max_frameset_hash])
-#            print(totals_this_movie)
+########### NEXT:
+#       for each movie
+#         get max_frameset_rows
+#         for each max_frameset_grid_element:
+#           find how many framesets did not see it.  
+#           if above a threshold*, this could be editable text
+#
+#       find a supermax frameset grid across the movies
+#       for each supermax frameset grid field:
+#           find out how many non-supermax movie max grids did not see it
+#           if above a threshold*, this is text that varies by client
+#
+#  *the above threshold is because text can randomly 'ocr' slightly differently
+#      like a lowercase L becomes a one, or a word gets split into two.
 
+
+        print('hippies, take a bath!')
+        print(self.all_movie_scores)
+        print(self.movie_max_frameset_rows)
 
         return {'donkey': 'cheese'}
 
@@ -52,6 +72,7 @@ class DataSifterCompiler(GridPointScorer):
             return # of course max matches itself 100%
         fs_rows = self.all_frameset_rows[movie_url][frameset_hash]
         max_fs_rows = self.all_frameset_rows[movie_url][max_frameset_hash]
+        self.movie_max_frameset_rows[movie_url] = max_fs_rows
         match_cache = {}
         rta_scores = {}
         for fs_row_index, row in enumerate(fs_rows):
@@ -63,6 +84,19 @@ class DataSifterCompiler(GridPointScorer):
                     if fs_row_index not in rta_scores:
                         rta_scores[fs_row_index] = {}
                     rta_scores[fs_row_index][fs_col_index] = score
+        if not rta_scores:
+            return 0
+        best_score_obj = {}
+        best_total_score = 0
+        for row in rta_scores:
+            for col in rta_scores[row]:
+                grid_ele = rta_scores[row][col]
+                if grid_ele['total_score'] > best_total_score:
+                    best_score_obj = grid_ele
+                    best_total_score = grid_ele['total_score']
+#        this looks like so:
+#        {'total_score': 1990, 'app_row_scores': [[82], [100], [91], [100, 0], [100, 86], [0, 89], [100, 0, 100], [0, 100, 0, 0], [100, 86], [100, 0, 82], [100, 95], [93, 100], [100], [0, 0, 86], [100]], 'app_row_rta_coords': [[(0, 0)], [(1, 0)], [(2, 0)], [(3, 0), (0, 0)], [(4, 0), (4, 1)], [(0, 0), (5, 1)], [(6, 1), (0, 0), (6, 2)], [(0, 0), (7, 1), (0, 0), (0, 0)], [(8, 0), (8, 1)], [(9, 0), (0, 0), (9, 1)], [(10, 0), (10, 1)], [(11, 0), (11, 1)], [(12, 0)], [(0, 0), (0, 0), (13, 1)], [(14, 0)]], 'window_start': [326, 424], 'window_end': [713, 858], 'app_home_coords': (0, 0), 'app_home_text': {'source': 'ocr: east+tess', 'location': [494, 225], 'size': [58, 17], 'origin': [], 'ocr_window_start': [311, 208], 'scale': 1, 'scanner_type': 'ocr', 'text': 'Alanidra'}, 'app_row_scores_primary_coords': [15, 0]}
+        return best_score_obj
 
     def score_one_row_token(
         self,
