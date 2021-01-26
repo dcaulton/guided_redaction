d2a41073e1216abe62f5ac17b8407ce7c2975dbc
Dave Caulton <dcaulton@gmail.com>
2021-01-25 21:08:43 -0600

    getting all the true/false pos/neg counts right for manual reviews

--- api/guided_redaction/job_run_summaries/controller_score_manual.py
+++ api/guided_redaction/job_run_summaries/controller_score_manual.py
@@ -104,15 +104,30 @@ class ScoreManualController(T1Controller):
         not_found_image = cv2.bitwise_not(found_image)
         tpos_url, tpos_image = self.draw_and_save_tpos_image(found_image, jrs_frameset_data, fsh_prefix)
         not_tpos_image = cv2.bitwise_not(tpos_image)
+
         fpos_url, fpos_image = self.draw_and_save_fpos_image(found_image, not_tpos_image, fsh_prefix)
 
-        fneg_url = self.draw_and_save_fneg_image(job_frameset_data, frame_dimensions, fsh_prefix)
-        fall_url = self.draw_and_save_fall_image(job_frameset_data, frame_dimensions, fsh_prefix)
+        fneg_url, fneg_image = self.draw_and_save_fneg_image(not_found_image, tpos_image, fsh_prefix)
+
+        fall_url, fall_image = self.draw_and_save_fall_image(fpos_image, fneg_image, fsh_prefix)
         maps = {
             't_pos': tpos_url,
             'f_pos': fpos_url,
             'f_neg': fneg_url,
             'f_all': fall_url,
+        }
+
+        total_pixels = int(frame_dimensions[0] * frame_dimensions[1])
+        num_tpos = np.count_nonzero(tpos_image)
+        num_tneg = total_pixels - num_tpos
+        num_fpos = np.count_nonzero(fpos_image)
+        num_fneg = np.count_nonzero(fneg_image)
+
+        counts = {
+            't_pos': num_tpos,
+            't_neg': num_tneg,
+            'f_pos': num_fpos,
+            'f_neg': num_fneg,
         }
 
         return counts, maps
@@ -161,39 +176,25 @@ class ScoreManualController(T1Controller):
         url = self.file_writer.write_cv2_image_to_filepath(fpos_image, fullpath)
         return url, fpos_image
 
-    def draw_and_save_fneg_image(self, job_frameset_data, frame_dimensions, fsh_prefix):
-        img = np.zeros((frame_dimensions[1], frame_dimensions[0], 1), np.uint8)
-        cv2.rectangle(
-            img,
-            (410, 10),
-            (430, 300),
-            255,
-            -1
-        )
+    def draw_and_save_fneg_image(self, not_found_image, tpos_image, fsh_prefix):
+        fneg_image = cv2.bitwise_and(not_found_image, tpos_image)
         file_name = 'f_neg_' + fsh_prefix + '.png'
         fullpath = self.file_writer.build_file_fullpath_for_uuid_and_filename(
             self.file_storage_dir_uuid,
             file_name
         ) 
-        url = self.file_writer.write_cv2_image_to_filepath(img, fullpath)
-        return url
+        url = self.file_writer.write_cv2_image_to_filepath(fneg_image, fullpath)
+        return url, fneg_image
 
-    def draw_and_save_fall_image(self, job_frameset_data, frame_dimensions, fsh_prefix):
-        img = np.zeros((frame_dimensions[1], frame_dimensions[0], 1), np.uint8)
-        cv2.rectangle(
-            img,
-            (510, 10),
-            (530, 300),
-            255,
-            -1
-        )
+    def draw_and_save_fall_image(self, fpos_image, fneg_image, fsh_prefix):
+        fall_image = cv2.bitwise_or(fpos_image, fneg_image)
         file_name = 'f_all_' + fsh_prefix + '.png'
         fullpath = self.file_writer.build_file_fullpath_for_uuid_and_filename(
             self.file_storage_dir_uuid,
             file_name
         ) 
-        url = self.file_writer.write_cv2_image_to_filepath(img, fullpath)
-        return url
+        url = self.file_writer.write_cv2_image_to_filepath(fall_image, fullpath)
+        return url, fall_image
 
     def draw_and_save_found_image(self, job_frameset_data, frame_dimensions, fsh_prefix):
         img = self.draw_job_matches(job_frameset_data, frame_dimensions)
