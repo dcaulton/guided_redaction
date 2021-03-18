23f8f83da5abdb36af3eb0116b9df479753975c1
Dave Caulton <dcaulton@gmail.com>
2021-03-18 15:16:31 -0500

    add shared ocr rowcol quantizing logic to manual compile for data sifter

--- api/guided_redaction/analyze/classes/DataSifterManualCompiler.py
+++ api/guided_redaction/analyze/classes/DataSifterManualCompiler.py
@@ -1,4 +1,4 @@
-import numpy as np
+from guided_redaction.analyze.classes.OcrRowColMaker import OcrRowColMaker
 
 
 class DataSifterManualCompiler():
@@ -9,19 +9,23 @@ class DataSifterManualCompiler():
             self.source_movies = movies['source']
         del movies['source']
         self.movies = movies
-        self.all_frameset_rows = {}
-        self.movie_max_frameset_rows = {}
-        self.all_movie_scores = {}
         self.debug = False
-        self.row_threshold = 20
-        self.word_match_threshold = 80
+        self.ocr_rowcol_maker = OcrRowColMaker()
 
     def compile(self):
-    # gather ocr_rows, left_cols, right_cols
-    # make guesses as to which fields are labels
-        print('POCKY WOCKY')
+        # gather ocr_rows, left_cols, right_cols
+        # make guesses as to which fields are labels
+        ocr_results_this_frame = {}
+        for movie_url in self.movies:
+            for frameset_hash in self.movies[movie_url]['framesets']:
+                ocr_results_this_frame = self.movies[movie_url]['framesets'][frameset_hash]
 
+        ocr_rows_dict = self.ocr_rowcol_maker.gather_ocr_rows(ocr_results_this_frame)
+        ocr_left_cols_dict, ocr_right_cols_dict = self.ocr_rowcol_maker.gather_ocr_cols(ocr_results_this_frame)
 
+        print('ocr rowwies {}'.format(ocr_rows_dict))
+        print('ocr left collars {}'.format(ocr_left_cols_dict))
+        print('ocr right collars {}'.format(ocr_right_cols_dict))
         
         return self.data_sifter
 
