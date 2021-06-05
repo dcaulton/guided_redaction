f2956fd0f3106794b1cd5a299deb6df1d0ffa9fa
Dave Caulton <dcaulton@gmail.com>
2021-06-04 21:09:54 -0500

    start returning match objects with focus finder

--- api/guided_redaction/analyze/classes/FocusFinder.py
+++ api/guided_redaction/analyze/classes/FocusFinder.py
@@ -1,3 +1,4 @@
+import random
 from fuzzywuzzy import fuzz
 
 class FocusFinder:
@@ -59,6 +60,8 @@ class FocusFinder:
 #        print('DONE new: {}'.format(new_match_ids))
         print('new: {} no change: {}, altered: {}, deleted: {}'.format(len(new_match_ids), len(no_change_match_ids), len(altered_match_ids), len(disappeared_prev_match_ids)))
 
+        match_obj = self.build_response_data(new_match_ids, no_change_match_ids, altered_match_ids, ocr_matches)
+
         return match_obj, match_stats, masks 
 
     def get_centroid(self, ocr_match_ele): 
@@ -74,3 +77,88 @@ class FocusFinder:
         if ocr_match_ele['location'][0] <= coords[0] <= ocr_end[0]:
             if ocr_match_ele['location'][1] <= coords[1] <= ocr_end[1]:
                 return True
+
+    def build_response_data(self, new_match_ids, no_change_match_ids, altered_match_ids, ocr_matches):
+        build_response_data = {}
+        env_start_x = False
+        env_end_x = False
+        env_start_y = False
+        env_end_y = False
+        for nid in new_match_ids:
+            ocr_ele = ocr_matches[nid]
+            ocr_end = (
+                ocr_ele['location'][0] + ocr_ele['size'][0],
+                ocr_ele['location'][1] + ocr_ele['size'][1]
+            )
+            if not env_start_x:
+                env_start_x = ocr_ele['location'][0]
+            elif ocr_ele['location'][0] < env_start_x:
+                env_start_x = ocr_ele['location'][0]
+            if not env_end_x:
+                env_end_x = ocr_end[0]
+            elif ocr_end[0] > env_end_x:
+                env_end_x = ocr_end[0]
+            if not env_start_y:
+                env_start_y = ocr_ele['location'][1]
+            elif ocr_ele['location'][1] < env_start_y:
+                env_start_y = ocr_ele['location'][1]
+            if not env_end_y:
+                env_end_y = ocr_end[1]
+            elif ocr_end[1] > env_end_y:
+                env_end_y = ocr_end[1]
+            build_ele = self.build_match_obj_for_field_type('new', ocr_ele)
+            build_response_data[build_ele['id']] = build_ele
+        for aid in altered_match_ids:
+            ocr_ele = ocr_matches[aid]
+            ocr_end = (
+                ocr_ele['location'][0] + ocr_ele['size'][0],
+                ocr_ele['location'][1] + ocr_ele['size'][1]
+            )
+            if not env_start_x:
+                env_start_x = ocr_ele['location'][0]
+            elif ocr_ele['location'][0] < env_start_x:
+                env_start_x = ocr_ele['location'][0]
+            if not env_end_x:
+                env_end_x = ocr_end[0]
+            elif ocr_end[0] > env_end_x:
+                env_end_x = ocr_end[0]
+            if not env_start_y:
+                env_start_y = ocr_ele['location'][1]
+            elif ocr_ele['location'][1] < env_start_y:
+                env_start_y = ocr_ele['location'][1]
+            if not env_end_y:
+                env_end_y = ocr_end[1]
+            elif ocr_end[1] > env_end_y:
+                env_end_y = ocr_end[1]
+            build_ele = self.build_match_obj_for_field_type('altered', ocr_ele)
+            build_response_data[build_ele['id']] = build_ele
+
+        if env_start_x and env_end_x and env_start_y and env_end_y:
+            the_id = 'ff_' + str(random.randint(100000000, 999000000))
+            location = (env_start_x, env_start_y)
+            size = (
+                env_end_x - env_start_x,
+                env_end_y - env_start_y
+            )
+            build_ele = {
+                'id': the_id,
+                'scanner_type': 'focus_finder',
+                'focus_object_type': 'app',
+                'location': location,
+                'size': size,
+            }
+            build_response_data[build_ele['id']] = build_ele
+
+        return build_response_data
+
+    def build_match_obj_for_field_type(self, field_type, ocr_ele):
+        the_id = 'ff_' + str(random.randint(100000000, 999000000))
+        build_ele = {
+            'id': the_id,
+            'scanner_type': 'focus_finder',
+            'focus_object_type': 'field',
+            'field_type': 'new',
+            'location': ocr_ele['location'],
+            'size': ocr_ele['size'],
+        }
+        return build_ele
