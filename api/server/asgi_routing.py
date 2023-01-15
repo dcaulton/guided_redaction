f396b7d71ef200a001228e690c81727c43cb9a55
Dave Caulton <dcaulton@gmail.com>
2023-01-14 20:34:58 -0600

    working towards using daphne through nginx right now that is through docker compose

--- api/server/asgi_routing.py
+++ api/server/asgi_routing.py
@@ -1,18 +1,23 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.wsgi import get_wsgi_application
+import server.asgi as local_asgi
 
 #import gt.asgi_routing
 #import inspector.asgi_routing
 #import scan_rule.asgi_routing
 import guided_redaction.asgi_routing
 from server.router import get_asgi_router
+import server.urls 
 
 asgi_router = get_asgi_router()
 wsgi_application = get_wsgi_application()
+asgi_application = local_asgi.application
  
 application = ProtocolTypeRouter({
-#    'http': wsgi_application,
-    'http': AuthMiddlewareStack(URLRouter(asgi_router.urls)),
+    'http': asgi_application,
+#    'http': AuthMiddlewareStack(URLRouter(asgi_router.urls)),
+#    'http': URLRouter(server.urls.router.urls),
+#    'http': AuthMiddlewareStack(URLRouter(server.urls.router.urls)),
     'websocket': AuthMiddlewareStack(URLRouter(asgi_router.urls))
 })
