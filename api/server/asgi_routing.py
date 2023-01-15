4d1ceb32007e03b90c9745b623cd4858842dbc94
Dave Caulton <dcaulton@gmail.com>
2023-01-14 21:39:32 -0600

    get the backend working with wsgi

--- api/server/asgi_routing.py
+++ api/server/asgi_routing.py
@@ -12,10 +12,10 @@ import server.urls
 
 asgi_router = get_asgi_router()
 wsgi_application = get_wsgi_application()
-asgi_application = local_asgi.application
+#asgi_application = local_asgi.application
  
 application = ProtocolTypeRouter({
-    'http': asgi_application,
+    'http': wsgi_application,
 #    'http': AuthMiddlewareStack(URLRouter(asgi_router.urls)),
 #    'http': URLRouter(server.urls.router.urls),
 #    'http': AuthMiddlewareStack(URLRouter(server.urls.router.urls)),
